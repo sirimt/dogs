3bb5e2646d57e0e3934d1c813bd99286ec4cd70e
Siri Sue <sirisue@administorsair3.lan>
2017-10-21 10:46:40 -0600

    render list

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogs: []
+    };
+  }
+  componentWillMount() {
+    axios.get('/api/dogs')
+      .then((response) => {
+        this.setState({
+          dogs: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
+    let dogNames = this.state.dogs.map( (dog) => {
+      return <li>{ dog.name }</li>
+    });
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Dog's        
-        </p>
-      </div>
+      <ul>
+        { dogNames }
+      </ul>
     );
   }
 }
