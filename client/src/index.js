95de6706ef7da1cae384e7cac72f0d12f976476f
Siri Sue <sirieliasen@gmail.com>
2018-02-08 18:46:58 -0700

    logger

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import reducers from './reducers/';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
